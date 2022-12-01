30b11fa2efa1cc3270cde3db61077303ed8f6dd4
Filip Kaniecki <f.kaniecki@outlook.com>
2022-12-01 21:17:58 +0100

    second commit

---
+++ src/test/java/com/pubsupom/pubsumom/OrderManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vectra.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
